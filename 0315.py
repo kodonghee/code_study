29a5fddc6ee313813cd1230265da25f32ff83b9e
kodonghee <qmdlfakrhf@naver.com>
2021-03-15 22:28:10 +0900

    Update 0315.py

--- 0315.py
+++ 0315.py
@@ -14,3 +14,23 @@ def linear_search(element, some_list):
         if some_list[i] == element:
             return i
     return None
+
+
+## 배열 내에서 특정 값 찾기 (이진 탐색)
+
+def binary_search(element, some_list):
+    ori_list = some_list
+    index = (len(some_list) -1) //2
+    while len(some_list) > 0:
+        if some_list[index] == element:
+            for i in range(len(ori_list)):
+                if ori_list[i] == element:
+                    return i
+        
+        elif some_list[index] < element:
+            some_list = some_list[index + 1:len(some_list)]
+            index = (len(some_list) -1) //2
+        else:
+            some_list = some_list[0:index]
+            index = (len(some_list) -1) //2
+    return None
