074c7a98118ce2321ce5daf97cf8fb798d52afdc
kodonghee <qmdlfakrhf@naver.com>
2021-03-29 16:35:56 +0900

    FindNodeWithData

--- DataStructure/LinkedList.py
+++ DataStructure/LinkedList.py
@@ -13,6 +13,27 @@ class LinkedList:
         self.head = None
         self.tail = None
 
+    def find_node_with_data(self, data):
+        """링크드 리스트에서 탐색 연산 메소드. 단, 해당 노드가 없으면 None을 리턴한다"""
+        iterator = self.head
+
+        while iterator is not None:
+            if iterator.data == data:
+                return iterator
+                break
+            iterator = iterator.next
+
+        return None
+
+    def find_node_at(self, index):
+        """"링크드 리스트 접근 연산 메소드. 파라미터 인덱스는 항상 있다고 가정"""
+        iterator = self.head
+
+        for _ in range(index):
+            iterator = iterator.next
+
+        return iterator
+
     def append(self, data):
         """링크드 리스트 추가 연산 메소드"""
         new_node = Node(data)
@@ -23,6 +44,21 @@ class LinkedList:
         else:   # 링크드 리스트가 비어 있지 않은 경우
             self.tail.next = new_node
             self.tail = new_node
+
+    def __str__(self):
+        """링크드 리스트를 문자열로 표현해서 리턴하는 메소드"""
+        res_str = "|"
+
+        # 링크드 리스트 안에 모든 노드를 돌기 위한 변수. 일단 가장 앞 노드로 정의한다.
+        iterator = self.head
+
+        # 링크드 리스트 끝까지 돈다
+        while iterator is not None:
+            # 각 노드의 데이터를 리턴하는 문자열에 더해준다
+            res_str += f" {iterator.data} |"
+            iterator = iterator.next
+
+        return res_str
 
 
 # 새로운 링크드 리스트 생성
@@ -35,9 +71,10 @@ my_list.append(5)
 my_list.append(7)
 my_list.append(11)
 
-# 링크드 리스트 출력
-iterator = my_list.head
+# 링크드 리스트 노드에 접근 (데이터 가지고 오기)
+print(my_list.find_node_at(3).data)
 
-while iterator is not None:
-    print(iterator.data)
-    iterator = iterator.next
+# 링크드 리스트 노드에 접근 (데이터 바꾸기)
+my_list.find_node_at(2).data = 13
+
+print(my_list)  # 전체 링크드 리스트 출력
